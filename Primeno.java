c733c5a24f7fb32c258640449a6ff788a18b2bc2
Pavithrasai <pavithrambe25@gmail.com>
2016-07-18 18:43:30 +0530

    Create Primeno.java

---
+++ Primeno.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Prime
+{
+public static void main(String[]args)
+{
+int n;
+int j;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+j=sc.nextInt();
+for(i=j;i<n;i++)
+{
+if(i%2==1)
+{
+System.out.println(+i);
+}
+}
+}
+}
